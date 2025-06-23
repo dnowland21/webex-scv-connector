b578be79e7fd0dfa9fc727376767108ff338925e
Donald Nowland <129020204+dnowland21@users.noreply.github.com>
2025-06-23 04:23:25 -0400

    ViteConfig Changes
    Changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,51 @@
 import { defineConfig } from 'vite';
+import inject from '@rollup/plugin-inject';
+import path from 'path';
 
 export default defineConfig({
-  server: {
-    port: 3000,
-    strictPort: true
-  },
+  base: './',
   build: {
     outDir: 'dist',
-    target: 'esnext'
-  }
+    sourcemap: true,
+    rollupOptions: {
+      external: [
+        '@webex/internal-plugin-metrics',
+        '@webex/internal-plugin-conversation',
+        '@webex/internal-plugin-user',
+        '@webex/webex-core',
+        '@webex/plugin-authorization',
+        '@webex/plugin-people',
+        '@webex/plugin-logger',
+        '@webex/plugin-metrics',
+        '@webex/plugin-phone',
+        '@webex/plugin-logger',
+        '@webex/web-calling-sdk', // in case
+      ]
+    }
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+      process: 'process/browser',
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  define: {
+    global: 'globalThis'
+  },
+  optimizeDeps: {
+    include: ['buffer', 'process'],
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      }
+    }
+  },
+  plugins: [
+    inject({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser',
+      global: ['globalThis', 'global']
+    })
+  ]
 });
