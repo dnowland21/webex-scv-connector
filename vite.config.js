bc7cb8f17d6346ba1ea6470df9c00125e4018601
Donald Nowland <129020204+dnowland21@users.noreply.github.com>
2025-06-23 04:42:55 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,32 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
+    target: 'esnext',
+    outDir: 'dist',
     rollupOptions: {
       external: [
         '@webex/internal-plugin-metrics',
         '@webex/web-media-effects',
-        'globalThis'
-      ]
+        '@webex/media-core',
+        '@webex/webrtc-core'
+      ],
+      output: {
+        globals: {
+          '@webex/internal-plugin-metrics': 'WebexInternalPluginMetrics',
+          '@webex/web-media-effects': 'WebMediaEffects',
+          '@webex/media-core': 'WebexMediaCore',
+          '@webex/webrtc-core': 'WebexWebRTC'
+        }
+      }
     }
   },
   optimizeDeps: {
-    exclude: [
-      '@webex/internal-plugin-metrics',
-      '@webex/web-media-effects'
-    ]
+    include: ['@webex/calling']
+  },
+  server: {
+    port: 3000,
+    open: true
   }
 });
